eb76e59f82c53d45cbc4df81a1086d74b4e653cf
ackerley1019 <dacker35@gmail.com>
2022-03-21 00:58:35 +0800

    style: fix indent.

--- app/models/user.go
+++ app/models/user.go
@@ -9,7 +9,7 @@ import (
 type User struct {
 	gorm.Model
 	ID        int64     `gorm:"primary_key; auto_increment" json:"id"`
-	Account   string    `gorm:"size:100; not null;unique"    json:"account"`
+	Account   string    `gorm:"size:100; not null;unique"   json:"account"`
 	Password  string    `gorm:"size:100; not null;"         json:"password"`
 	Email     string    `gorm:"size:100; not null; unique"  json:"email"`
 	CreatedAt time.Time `gorm:"autoCreateTime"              json:"created_at"`
